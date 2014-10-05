f04a4dd62d98933879c9c2262011960444a9cc49
steventhanna <steventhanna@gmail.com>
2014-10-05 13:20:16 -0500

    Initial commit

---
+++ DataSimulate/src/DataSimulate.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2008-2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import com.mongodb.*;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * The tutorial from http://docs.mongodb.org/ecosystem/tutorial/getting-started-with-java-driver/
+ */
+public class DataSimulate extends Randomness {
+    // CHECKSTYLE:OFF
+    /**
+     * Run this main method to see the output of this quick example.
+     *
+     * @param args takes no args
+     * @throws IOException 
+     */
+    public static void main(final String[] args) throws IOException {
+        // connect to the local database server
+        MongoClient mongoClient = new MongoClient();
+
+        /*
+        // Authenticate - optional
+        MongoCredential credential = MongoCredential.createMongoCRCredential(userName, database, password);
+        MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+        */
+
+        // get handle to "mydb"
+        DB db = mongoClient.getDB("awf_db");
+
+        // get a list of the collections in this database and print them out
+        Set<String> collectionNames = db.getCollectionNames();
+        for (final String s : collectionNames) {
+            System.out.println(s);
+        }
+        randomDate();
+        randomFirstName();
+        randomLastName();
+        randomBoolean();
+        // get a collection object to work with
+        DBCollection coll = db.getCollection("influencer");
+        
+        System.out.println(coll);
+        /*
+        BasicDBObject doc = new BasicDBObject("_id", "josh")
+        .append("isActive", "true")
+        .append("sphere", "Politics")
+        .append("allowExplicit", "true")
+        .append("totalValue", 0)
+        .append("totalTweets", 0)
+        .append("payoutOption", 50);
+        coll.insert(doc);
+        DBObject myDoc = coll.findOne();
+        System.out.println(myDoc);
+         */
+        mongoClient.close();
+    }
+    // CHECKSTYLE:ON
+}
---
+++ DataSimulate/src/Randomness.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
+public class Randomness {
+	
+	public static void randomDate() {
+		Random random = new Random();
+		// Pick Month
+		int month = random.nextInt(13);
+		// Pick Date
+		int date = random.nextInt(32);
+		// Pick Year
+		int year = random.nextInt(2016);
+		// Output
+		System.out.println(month + "/" + date + "/" + year);
+		
+	}
+	
+	public static String randomFirstName() throws IOException {
+		String fileName = "/Users/steventhanna/Desktop/firstNames.txt";
+		ArrayList<String> allNames = new ArrayList<String>();
+		Random random = new Random();
+
+		BufferedReader in = new BufferedReader(new FileReader(fileName)); 
+		while (in.ready()) { 
+		     allNames.add( in.readLine() );
+		}
+		in.close();
+
+		String randomName = allNames.get(random.nextInt(allNames.size()));
+		System.out.println(randomName);
+		return randomName;
+	}
+	
+	public static String randomLastName() throws IOException {
+		String fileName = "/Users/steventhanna/Desktop/lastNames.txt";
+		ArrayList<String> allNames = new ArrayList<String>();
+		Random random = new Random();
+
+		BufferedReader in = new BufferedReader(new FileReader(fileName)); 
+		while (in.ready()) { 
+		     allNames.add( in.readLine() );
+		}
+		in.close();
+
+		String randomName = allNames.get(random.nextInt(allNames.size()));
+		System.out.println(randomName);
+		return randomName;
+	}
+	
+	public static boolean randomBoolean() {
+		Random random = new Random();
+		Boolean bool = random.nextBoolean();
+		System.out.println(bool);
+		return bool;
+	}
+	
+	
+}
